fcba25f3a2be6385e065acf40c7233bbb571046a
patrickHub <patrick.djomo@heig-vd.ch>
2019-09-24 10:32:12 +0200

    add script for dynamically manage url

---
+++ private/functions.php
@@ -0,0 +1,38 @@
+<?php
+
+    function url_for($script_path)
+    {
+
+        // check if script_path starts with '/'
+        if ($script_path[0] != "/") {
+            $script_path  = "/" . $script_path;
+        }
+        return WWW_ROOT . $script_path;
+    }
+
+    function u($string = "")
+    {
+        return urlencode($string);
+    }
+
+    function raw_u($string = "")
+    {
+        return rawurlencode($string);
+    }
+
+    function h($string = "")
+    {
+        return htmlspecialchars($string);
+    }
+
+    function error_404()
+    {
+        header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+        exit();
+    }
+
+    function error_500()
+    {
+        header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+        exit();
+    }
