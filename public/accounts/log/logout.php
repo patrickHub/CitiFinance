f5510153c61eb7815ffcb17be0ed77dcd45c23f4
patrickHub <patrick.djomo@heig-vd.ch>
2019-11-20 15:25:11 +0100

    add logout functionnality + some css refactoring

---
+++ public/accounts/log/logout.php
@@ -0,0 +1,11 @@
+<?php require_once('../../../private/initialize.php');  ?>
+<?php $page_title = 'Login';  ?>
+<?php
+
+    $login_succeed_message = null;
+
+    log_out();
+    redirect_to(url_for('/index.php'));
+
+
+?>
